138950eb08af42043f430ff4bf355838937b2367
DESKTOP-4I0Q9AM\user1 <skan64554@gmail.com>
2026-02-20 12:51:54 +0900

    서블릿 2일차 오후 공부는 집에서

--- 02_helloJsp/src/main/java/com/kh/action/ElServlet.java
+++ 02_helloJsp/src/main/java/com/kh/action/ElServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ElServlet
  */
-@WebServlet("/ElServlet")
+@WebServlet("/el/el.do")
 public class ElServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
